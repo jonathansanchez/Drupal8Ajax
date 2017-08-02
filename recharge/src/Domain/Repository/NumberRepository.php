0868425ffe163a749f803c2b3bb308f32c7dc381
Jonathan <jsanchezorellana@gmail.com>
2017-08-02 18:21:43 -0400

    Created Interface that is implemented by Infrastructure

---
+++ recharge/src/Domain/Repository/NumberRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\recharge\Domain\Repository;
+
+use Drupal\recharge\Domain\Entity\Number;
+
+/**
+ * Interface Recharge
+ * @package Domain\Repository
+ *
+ * This file must be implemented by infrastructure
+ * REST API to persist phone number.
+ */
+interface NumberRepository
+{
+    /**
+     * Recharge with amount a phone number
+     *
+     * @param Number $number
+     *
+     * @return bool true|false
+     */
+    public function recharge(Number $number);
+}
